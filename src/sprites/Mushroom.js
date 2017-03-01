5b3d733ca659d59b51b76e2fc4b94d320919b9ba
Danny Peck <dannytpeck@gmail.com>
2017-03-01 14:57:19 -0800

    Remove unused mushroom code

--- src/sprites/Mushroom.js
+++ src/sprites/Mushroom.js
@@ -1,3 +1,8 @@
+/* Just leaving this in as an example of a custom Sprite.
+   We can make custom sprites for all sprites, but I think
+   that might not be worth doing. Probably best just to
+   use custom sprites for sprites with actual behavior */
+
 import Phaser from 'phaser';
 
 export default class extends Phaser.Sprite {
