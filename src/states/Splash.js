bdfc8cfde0c4e29a191e20b57659bde9399c0d7a
Danny Peck <dannytpeck@gmail.com>
2017-03-14 21:57:09 +0000

    Add tiled support

--- src/states/Splash.js
+++ src/states/Splash.js
@@ -14,12 +14,19 @@ class SplashState extends Phaser.State {
     // load your assets
     //
     this.load.image('sky', 'assets/images/sky.png');
+    this.load.image('background', 'assets/images/background2.png');
     this.load.image('ground', 'assets/images/platform.png');
     this.load.image('star', 'assets/images/star.png');
+    this.load.image('star2', 'assets/images/star2.png');
+    this.load.image('tiles-1', 'assets/images/tiles-1.png');
+    
     this.load.spritesheet('dude', 'assets/images/dude.png', 32, 48);
+    this.load.spritesheet('droid', 'assets/images/droid.png', 32, 32);
+    
+    this.load.tilemap('level1', 'assets/tiled/level1.json', null, Phaser.Tilemap.TILED_JSON);
   }
 
-  create () {
+  create() {
     this.state.start('Game');
   }
 
